fabd5dc1f992b424ac93df034c336fb85b7075f5
Yufanggg <wangalisa2017@gmail.com>
2025-02-26 17:34:36 +0100

    simplify the code

--- SurvivalDataGenerator.R
+++ SurvivalDataGenerator.R
@@ -2,59 +2,38 @@ set.seed(50234)
 library(KMsurv)
 library(survival)
 
-SurvivalDataGenerator = function(n, distribution = "exponential", 
-                                 anycovariate = "no", beta = "no"){
-  if (distribution == "exponential" & anycovariate == "no") {
-    # simulate survival time from an exponential distribution
-    survtime <- rexp(n, 0.2)
-    covariate <- NA
-    K <- 0
-  }
-  else if (distribution == "exponential" & anycovariate == "binary"){
-    # simulate survival time from an exponential distribution
-    covariate <- factor(rep(c("F", "M"), each = n/2))
-    lambdas = exp(beta * as.numeric(covariate))
-    survtime <- sapply(lambdas, function(lambda) rexp(1, lambda))
-    intercept <- matrix(1, nrow = n, ncol = 1)
-    design_matrix <- cbind(intercept, covariate)
-    K = 2
+SurvivalDataGenerator = function(n_samples, Betas, Design_matrix, BaselinePara, BaselineDistr = "exponential"){
+  if (BaselineDistr == "exponential"){
+    lambda = BaselinePara
+    Baseline_survival_time = rexp(n_samples, rate = lambda)
   }
   
-  else if (distribution == "exponential" & anycovariate == "continuous"){
-    covariate <- rnorm(1000)
-    lambdas <- exp(beta * covariate)
-    survtime <- sapply(lambdas, function(lambda) rexp(1, lambda))
-    intercept <- matrix(1, nrow = n, ncol = 1)
-    design_matrix <- cbind(intercept, covariate)
-    K = 1
+  else if (BaselineDistr == "weibull"){
+    shape = BaselinePara[1]; scale = BaselinePara[2]
+    Baseline_survival_time = rweibull(n_samples, shape = shape,  scale = scale)
+    
   }
   
-  else if (distribution == "exponential_" & anycovariate == "continuous"){
-    covariate <- rnorm(1000)
-    lambdas <- exp(beta * covariate)
-    survtime <- sapply(lambdas, function(lambda) rexp(1, lambda))
-    design_matrix <- as.matrix(covariate)
-    K = 1
-  }
+  Survival_time_covarite = Baseline_survival_time * exp(Design_matrix %*% Betas)
   
   # simulate censored data from a uniform distribution
-  censtime <- runif(n, 3, 6)
+  censtime <- runif(n_samples, 3, 6)
   
   # indicator variable
-  status <- as.numeric(survtime <= censtime) # the sum of status is the number of event
+  status <- as.numeric(Survival_time_covarite <= censtime) # the sum of status is the number of event
   
   # observed time
-  obstime <- survtime * status + censtime * (1 - status) 
+  obstime <- Survival_time_covarite * status + censtime * (1 - status) 
   
   stan_data <- list(
     N = sum(status == 1),
-    t = survtime[status == 1],
+    t = Survival_time_covarite[status == 1],
     N_cens = sum(status == 0),
     t_cens = censtime[status == 0], # Censoring time used in the simulation
-    K = K, # number of covariates,
-    x = as.matrix(design_matrix[status == 1,]),
-    x_cens = as.matrix(design_matrix[status == 0,]),
-    covariate = covariate,
+    K = dim(Design_matrix)[2], # number of covariates,
+    x = as.matrix(Design_matrix[status == 1,]),
+    x_cens = as.matrix(Design_matrix[status == 0,]),
+    covariate = Design_matrix,
     obstime = obstime,
     status = status
   )
