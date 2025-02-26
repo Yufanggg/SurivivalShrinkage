c68cabf5f1a43b5e47305ba4454ea77454e86812
Yufanggg <wangalisa2017@gmail.com>
2025-02-26 11:50:10 +0100

    validated except the PH model

--- SurvivalDataGenerator.R
+++ SurvivalDataGenerator.R
@@ -8,20 +8,36 @@ SurvivalDataGenerator = function(n, distribution = "exponential",
     # simulate survival time from an exponential distribution
     survtime <- rexp(n, 0.2)
     covariate <- NA
+    K <- 0
   }
   else if (distribution == "exponential" & anycovariate == "binary"){
     # simulate survival time from an exponential distribution
-    survtime <- c(rexp(n/2, 0.2), rexp(n/2, 0.2 + beta))
     covariate <- factor(rep(c("F", "M"), each = n/2))
+    lambdas = exp(beta * as.numeric(covariate))
+    survtime <- sapply(lambdas, function(lambda) rexp(1, lambda))
+    intercept <- matrix(1, nrow = n, ncol = 1)
+    design_matrix <- cbind(intercept, covariate)
+    K = 2
   }
   
   else if (distribution == "exponential" & anycovariate == "continuous"){
-    covariate = rnorm(1000)
-    lambdas = exp(beta * covariate)
+    covariate <- rnorm(1000)
+    lambdas <- exp(beta * covariate)
     survtime <- sapply(lambdas, function(lambda) rexp(1, lambda))
+    intercept <- matrix(1, nrow = n, ncol = 1)
+    design_matrix <- cbind(intercept, covariate)
+    K = 1
   }
   
-  # simulate censored data from an exponential distribution
+  else if (distribution == "exponential_" & anycovariate == "continuous"){
+    covariate <- rnorm(1000)
+    lambdas <- exp(beta * covariate)
+    survtime <- sapply(lambdas, function(lambda) rexp(1, lambda))
+    design_matrix <- as.matrix(covariate)
+    K = 1
+  }
+  
+  # simulate censored data from a uniform distribution
   censtime <- runif(n, 3, 6)
   
   # indicator variable
@@ -31,13 +47,18 @@ SurvivalDataGenerator = function(n, distribution = "exponential",
   obstime <- survtime * status + censtime * (1 - status) 
   
   stan_data <- list(
-    N = n,
-    t = obstime,
+    N = sum(status == 1),
+    t = survtime[status == 1],
     N_cens = sum(status == 0),
-    t_cens = censtime, # Censoring time used in the simulation
-    covariate = covariate
+    t_cens = censtime[status == 0], # Censoring time used in the simulation
+    K = K, # number of covariates,
+    x = as.matrix(design_matrix[status == 1,]),
+    x_cens = as.matrix(design_matrix[status == 0,]),
+    covariate = covariate,
+    obstime = obstime,
+    status = status
   )
-  return (stan_Data)
+  return (stan_data)
 }
 
 
